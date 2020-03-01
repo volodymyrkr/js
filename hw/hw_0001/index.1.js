26623618ddd796b9c6b68129ef6cb616591d34ff
Nedava22 <nedava.dmitriy.22@gmail.com>
2020-03-01 17:59:47 +0200

    Feature: Сompleted task 1

--- hw/hw_0001/index.1.js
+++ hw/hw_0001/index.1.js
@@ -1,17 +1,22 @@
 let persons = [
-    {name: "Volodymyr", yearsOfBirth: 1981},
-    {name: "Elena", yearsOfBirth: 1988},
-    {name: "Steshka", yearsOfBirth: 2013},
-    {name: "Emilie", yearsOfBirth: 2015},
+    {name: "Volodymyr", surname: "Kravchenko" , yearsOfBirth: 1981},
+    {name: "Elena", surname: "Kravchenko" , yearsOfBirth: 1988},
+    {name: "Steshka",surname:"Kravchenko", yearsOfBirth: 2013},
+    {name: "Emilie",surname:"Kravchenko", yearsOfBirth: 2015},
+    {name: "Dmytro", surname:"Nedava" , yearsOfBirth: 2002},
+    {name: "Lena", surname:"Nedava" ,yearsOfBirth: 1974},
+    {name: "Sergey",surname:"Nedava" ,yearsOfBirth: 1974},
+    {name: "Nastya",surname:"Nedava" ,yearsOfBirth: 1996},
+    {name: "Nikita", surname:"Nedava" , yearsOfBirth: 2013},
 ];
 
 var date = new Date();
 var currentYear = date.getFullYear();
 
-console.log("My family:");
+console.log("My relatives:");
 
 persons.forEach((person) => {
-    console.log("-",person.name, currentYear - person.yearsOfBirth);
+    console.log("-",person.name, person.surname,currentYear - person.yearsOfBirth,);
 });
 
 var totalAmountOfYears = persons.reduce((prev, person) => {
