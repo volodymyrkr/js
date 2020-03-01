46d36094fc19b103a2da3e00b3e5b0a2f9a7510d
Nedava22 <nedava.dmitriy.22@gmail.com>
2020-03-01 19:39:30 +0200

    Feature: Сompleted task 3

--- hw/hw_0001/index.3.js
+++ hw/hw_0001/index.3.js
@@ -1,13 +1,22 @@
 let currencies = [
-    {name: "hrn", cost: 1},
-    {name: "usd", cost: 25.2},
-    {name: "eur", cost: 30.1},
+    {sortname: "AUD",name: "Австралійський долар",cost: 16.155},
+    {sortname: "AZD",name: "Азербайджанський манат",cost: 14.4655},
+    {sortname: "BYN",name: "Бiлоруський рубль", cost: 10.9976 },
+    {sortname: "HKD",name: "Гонконгівський долар",cost: 3.1511},
+    {sortname: "DKK",name: "Данська крона",cost: 3.6037},
+    {sortname: "USD",name: "Долар США",cost: 24.561},
+    {sortname: "EUR",name: "Євро",cost: 26.9299},
+    {sortname: "EGP",name: "Єгипетський фунт",cost: 1.5739},
+    {sortname: "PLN",name: "Злотий",cost: 6.2426},
+    {sortname: "CAD",name: "Канадський долар",cost: 18.3881},
+    {sortname: "HRK",name: "Куна",cost: 3.6095},
+    {sortname: "MDL",name: "Молдовський лей",cost: 1.3754},
 ];
 
 console.log("Currencies:");
 
 currencies.forEach((currency) => {
-    console.log(`- ${currency.name} costs ${currency.cost} hrn`);
+    console.log(`- ${currency.sortname} costs ${currency.cost} hrn (${currency.name})`);
 });
 
 var totalCost = currencies.reduce((prev, currency) => {
