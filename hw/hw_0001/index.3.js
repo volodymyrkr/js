8b2fc38b8c090ad56aa78e0f560e78ef3806bc6c
Nedava22 <nedava.dmitriy.22@gmail.com>
2020-03-07 13:26:19 +0200

    Bugfix: Some stupid changes

--- hw/hw_0001/index.3.js
+++ hw/hw_0001/index.3.js
@@ -1,22 +1,22 @@
 let currencies = [
-    {sortname: "AUD",name: "Австралійський долар",cost: 16.155},
-    {sortname: "AZD",name: "Азербайджанський манат",cost: 14.4655},
-    {sortname: "BYN",name: "Бiлоруський рубль", cost: 10.9976 },
-    {sortname: "HKD",name: "Гонконгівський долар",cost: 3.1511},
-    {sortname: "DKK",name: "Данська крона",cost: 3.6037},
-    {sortname: "USD",name: "Долар США",cost: 24.561},
-    {sortname: "EUR",name: "Євро",cost: 26.9299},
-    {sortname: "EGP",name: "Єгипетський фунт",cost: 1.5739},
-    {sortname: "PLN",name: "Злотий",cost: 6.2426},
-    {sortname: "CAD",name: "Канадський долар",cost: 18.3881},
-    {sortname: "HRK",name: "Куна",cost: 3.6095},
-    {sortname: "MDL",name: "Молдовський лей",cost: 1.3754},
+    {shortname: "AUD", name: "Австралійський долар", cost: 16.155},
+    {shortname: "AZD", name: "Азербайджанський манат", cost: 14.4655},
+    {shortname: "BYN", name: "Бiлоруський рубль", cost: 10.9976},
+    {shortname: "HKD", name: "Гонконгівський долар", cost: 3.1511},
+    {shortname: "DKK", name: "Данська крона", cost: 3.6037},
+    {shortname: "USD", name: "Долар США", cost: 24.561},
+    {shortname: "EUR", name: "Євро", cost: 26.9299},
+    {shortname: "EGP", name: "Єгипетський фунт", cost: 1.5739},
+    {shortname: "PLN", name: "Злотий", cost: 6.2426},
+    {shortname: "CAD", name: "Канадський долар", cost: 18.3881},
+    {shortname: "HRK", name: "Куна", cost: 3.6095},
+    {shortname: "MDL", name: "Молдовський лей", cost: 1.3754},
 ];
 
 console.log("Currencies:");
 
 currencies.forEach((currency) => {
-    console.log(`- ${currency.sortname} costs ${currency.cost} hrn (${currency.name})`);
+    console.log(`- ${currency.shortname} costs ${currency.cost} hrn (${currency.name})`);
 });
 
 var totalCost = currencies.reduce((prev, currency) => {
