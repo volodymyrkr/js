3f319d78ff540ead78d2d63606e984d6aefcda38
Nedava22 <nedava.dmitriy.22@gmail.com>
2020-03-13 12:03:24 +0200

    Hw_task_02: Completed task 2

--- hw/hw_0001/index.2.js
+++ hw/hw_0001/index.2.js
@@ -1,21 +1,28 @@
-let goods = [
-    {name: "Beer", cost: 1.5},
-    {name: "Milk", cost: 2.5},
-    {name: "Bread", cost: 4.5},
-    {name: "Butter", cost: 1.5},
-    {name: "Sausages", cost: 3},
+let products = [
+  {name: "Beer", number: "0,5L", price: 17.90},
+  {name: "Butter", number: "400g",price: 30.5},
+  {name: "Sausages", number: "400g",price: 50.3},
+  {name: "Eggs", number: "10 pieces", price: 20.55},
+  {name: "Pepsi", number: "2L", price: 23.9},
+  {name: "Bread", number: "400g", price: 12.8},
+  {name: "Kinkali", number: "0.8kg", price: 39.95},
+  {name: "Milk", number: "0.9kg", price: 38.8},
+  {name: "Wheat flour", number: "2kg", price: 42.95},
+  {name: "Condensed milk", number: "300g", price: 79.35},
+  {name: "White chocolate", number: "100g", price: 74.7},
+  {name: "Sugar", number: "1kg", price: 14.5},
+  {name: "Oatmeal", number: "1kg", price: 23.3},
+  {name: "Marshmallows", number: "500g", price: 45.85},
 ];
-
-var date = new Date();
 
 console.log("My buyings:");
 
-goods.forEach((good) => {
-    console.log(`- ${good.name} costs ${good.cost}`);
+products.forEach((product) => {
+  console.log(`- ${product.name} ${product.number} costs ${product.price}`);
 });
 
-var totalCost = goods.reduce((prev, good) => {
-    return prev + good.cost;
+var totalCost = products.reduce((prev, product) => {
+  return prev + product.price;
 }, 0);
 
-console.log("Total cost: ", totalCost);
+console.log(`Total cost ${totalCost} UAH`);
