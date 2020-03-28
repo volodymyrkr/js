9c750993d74deaacf973fcffd7085318dfbaced7
vladimirkr <vladimir.kravchenko@playtech.com>
2020-03-28 04:18:32 +0200

    Improvement: output formatting

--- hw/hw_0001/index.1.js
+++ hw/hw_0001/index.1.js
@@ -1,26 +1,26 @@
 let persons = [
-  {firstName: "Volodymyr", lastName: "Kravchenko" , yearsOfBirth: 1981},
-  {firstName: "Elena", lastName: "Kravchenko" , yearsOfBirth: 1988},
-  {firstName: "Steshka", lastName: "Kravchenko", yearsOfBirth: 2013},
-  {firstName: "Emilie", lastName: "Kravchenko", yearsOfBirth: 2015},
-  {firstName: "Dmytro", lastName: "Nedava" , yearsOfBirth: 2002},
-  {firstName: "Lena", lastName: "Nedava" , yearsOfBirth: 1974},
-  {firstName: "Sergey", lastName: "Nedava" , yearsOfBirth: 1974},
-  {firstName: "Nastya", lastName: "Nedava" , yearsOfBirth: 1996},
-  {firstName: "Nikita", lastName: "Nedava" , yearsOfBirth: 2013},
+  {firstName: "Volodymyr", lastName: "Kravchenko" , yearOfBirth: 1981},
+  {firstName: "Elena", lastName: "Kravchenko" , yearOfBirth: 1988},
+  {firstName: "Steshka", lastName: "Kravchenko", yearOfBirth: 2013},
+  {firstName: "Emilie", lastName: "Kravchenko", yearOfBirth: 2015},
+  {firstName: "Dmytro", lastName: "Nedava" , yearOfBirth: 2002},
+  {firstName: "Lena", lastName: "Nedava" , yearOfBirth: 1974},
+  {firstName: "Sergey", lastName: "Nedava" , yearOfBirth: 1974},
+  {firstName: "Nastya", lastName: "Nedava" , yearOfBirth: 1996},
+  {firstName: "Nikita", lastName: "Nedava" , yearOfBirth: 2013},
 ];
 
 var date = new Date();
 var currentYear = date.getFullYear();
 
-console.log("My relatives:");
+console.log(`FULLNAME`.padEnd(25, " "), `AGE`.padStart(7, " "));
 
 persons.forEach((person) => {
-  console.log(`- ${person.firstName} ${person.lastName} ${currentYear - person.yearsOfBirth}`);
+  console.log(`${person.firstName} ${person.lastName}`.padEnd(25, " "), `${currentYear - person.yearOfBirth} y.o.`.padStart(8, " "));
 });
 
 var totalAmountOfYears = persons.reduce((prev, person) => {
-  return prev + currentYear - person.yearsOfBirth;
+  return prev + currentYear - person.yearOfBirth;
 }, 0);
 
-console.log("Total amount of years: ", totalAmountOfYears);
+console.log(`\nTotal amount of years:`, totalAmountOfYears.toString().padStart(10, " "));
